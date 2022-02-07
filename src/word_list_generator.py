7818cbdfdea38c97abe373b96c01163e79d2d83a
Oskari Timgren <oskari.timgren@gmail.com>
2022-02-06 18:07:36 -0600

    Finnish translation of some parts

---
+++ src/word_list_generator.py
@@ -0,0 +1,13 @@
+"""
+Contains a program that is used to generate the list of acceptable words in
+Finnish based on the Finnish WordNet files available at:
+https://korp.csc.fi/download/FinnWordNet/v2.0/
+"""
+from pathlib import Path
+from typing import Union
+
+
+def generate_word_list(path_to_folder: Union[str, Path])
+
+
+if __name__ == "__main__":
